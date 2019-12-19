6d23bbf436995be2ed0f4ec4dd28f3862cfbb893
Sophie <strochain@gmail.com>
2019-12-19 18:02:00 +0100

    button bold, italic, title added

--- marcelup/src/Components/Converter.js
+++ marcelup/src/Components/Converter.js
@@ -7,40 +7,56 @@ const md = new MarkdownIt();
 class Converter extends Component {
   constructor(props) {
     super(props);
+    this.myRef = React.createRef();
     this.state = {
+      inputText: '',
       result: ''
     };
   }
-
-  change = e => {
-    let text = e.target.value;
-    let result = md.render(text);
+  insertText = (textToInsert) => {
+    const cursorPosition = this.myRef.current.selectionStart;
+    const currentText = this.state.inputText;
+    const textBeforeCursorPosition = currentText.substring(0, cursorPosition);
+    const textAfterCursorPosition = currentText.substring(cursorPosition, currentText.length);
+    this.updateState(textBeforeCursorPosition + textToInsert + textAfterCursorPosition);
+  };
+  updateState = (newText) => {
+    const result = md.render(newText);
     this.setState({
+      inputText: newText,
       result: result
     });
   };
 
+  updateChange = e => {
+    this.updateState(e.target.value);
+  };
+
   render() {
     return (
-      <div>
-        <form>
-          <textarea
-            id="markdown"
-            name="markdown"
-            rows="15"
-            cols="33"
-            onChange={this.change}
-          ></textarea>
-          <textarea
-            id="html"
-            name="html"
-            value={this.state.result}
-            readOnly
-            rows="15"
-            cols="33"
-          ></textarea>
-        </form>
-      </div>
+      <form>
+        <button type="button" onClick={ () => this.insertText('# \n') }>T1</button>
+        <button type="button" onClick={ () => this.insertText('## \n') }>T2</button>
+        <button type="button" onClick={ () => this.insertText('**\n') }>B</button>
+        <button type="button" onClick={ () => this.insertText('_\n') }>I</button>
+        <textarea
+          ref={this.myRef}
+          id="markdown"
+          name="markdown"
+          rows="15"
+          cols="33"
+          value={this.state.inputText}
+          onChange={this.updateChange}
+        ></textarea>
+        <textarea
+          id="html"
+          name="html"
+          value={this.state.result}
+          readOnly
+          rows="15"
+          cols="33"
+        ></textarea>
+      </form>
     );
   }
 }
