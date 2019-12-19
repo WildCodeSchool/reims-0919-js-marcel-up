362e4c7749a6b6d657b7e74ed0e557baf70c5a8e
Christopher Poncelet <christopher.poncelet51@gmail.com>
2019-12-20 00:39:00 +0100

    css to buttons

--- marcelup/src/Components/Converter.js
+++ marcelup/src/Components/Converter.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Component } from 'react';
 import './Converter.css';
 
-
 const MarkdownIt = require('markdown-it');
 const md = new MarkdownIt();
-
 
 class Converter extends Component {
   constructor(props) {
@@ -14,28 +12,40 @@ class Converter extends Component {
     this.state = {
       inputText: '',
       result: '',
-      count:  0
-
+      count: 0
     };
     this.nbOfWords = this.nbOfWords.bind(this);
   }
-  insertTextOne = (textToInsert) => {
+  insertTextOne = textToInsert => {
     const cursorPosition = this.myRef.current.selectionStart;
     const currentText = this.state.inputText;
     const textBeforeCursorPosition = currentText.substring(0, cursorPosition);
-    const textAfterCursorPosition = currentText.substring(cursorPosition, currentText.length);
-    this.updateState(textBeforeCursorPosition + textToInsert + textAfterCursorPosition);
+    const textAfterCursorPosition = currentText.substring(
+      cursorPosition,
+      currentText.length
+    );
+    this.updateState(
+      textBeforeCursorPosition + textToInsert + textAfterCursorPosition
+    );
   };
 
-  insertTextTwo = (textToInsert) => {
+  insertTextTwo = textToInsert => {
     const cursorPosition = this.myRef.current.selectionStart;
     const currentText = this.state.inputText;
     const textBeforeCursorPosition = currentText.substring(0, cursorPosition);
-    const textAfterCursorPosition = currentText.substring(cursorPosition, currentText.length);
-    this.updateState(textBeforeCursorPosition + textToInsert + textAfterCursorPosition + textToInsert);
+    const textAfterCursorPosition = currentText.substring(
+      cursorPosition,
+      currentText.length
+    );
+    this.updateState(
+      textBeforeCursorPosition +
+        textToInsert +
+        textAfterCursorPosition +
+        textToInsert
+    );
   };
 
-  updateState = (newText) => {
+  updateState = newText => {
     const result = md.render(newText);
     this.setState({
       inputText: newText,
@@ -48,32 +58,43 @@ class Converter extends Component {
   };
 
   nbOfWords() {
-  let table = this.state.result.split(/[.|,|?|!|;|#|*|-|' ']/g)
-  let count = table.length 
-  this.setState({
-    count
-  })  
-}
-  
+    let table = this.state.result.split(/[.|,|?|!|;|#|*|-|' ']/g);
+    let count = table.length;
+    this.setState({
+      count
+    });
+  }
+
   render() {
     return (
       <div>
         <form className="convert">
-          <label for="markdown">Start writing in Markdown :</label>
+          <label htmlFor="markdown">Start writing in Markdown :</label>
+          <div className="divButton">
+            <button type="button" onClick={() => this.insertTextOne('# ')}>
+              T1
+            </button>
+            <button type="button" onClick={() => this.insertTextOne('## ')}>
+              T2
+            </button>
+            <button type="button" onClick={() => this.insertTextTwo('**')}>
+              B
+            </button>
+            <button type="button" onClick={() => this.insertTextTwo('_')}>
+              I
+            </button>
+          </div>
           <textarea
-
             className="textMark"
-
             ref={this.myRef}
             id="markdown"
             name="markdown"
             rows="15"
             cols="33"
             value={this.state.inputText}
-
             onChange={this.change}
           ></textarea>
-          <label for="html">Converted in HTML :</label>
+          <label htmlFor="html">Converted in HTML :</label>
           <textarea
             className="textHtml"
             id="html"
@@ -82,15 +103,10 @@ class Converter extends Component {
             readOnly
           ></textarea>
         </form>
-
-        <button type="button" onClick={ () => this.insertTextOne('# ') }>T1</button>
-        <button type="button" onClick={ () => this.insertTextOne('## ') }>T2</button>
-        <button type="button" onClick={ () => this.insertTextTwo('**') }>B</button>
-        <button type="button" onClick={ () => this.insertTextTwo('_') }>I</button>
-
-        <button onClick={this.nbOfWords}>count</button>
-        <p>{this.state.count}</p>
-
+        <div className="buttonCount">
+          <button onClick={this.nbOfWords}>count</button>
+          <p>{this.state.count}</p>
+        </div>
       </div>
     );
   }
